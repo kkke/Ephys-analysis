69f4e6be0d10dc7ae4b5571aee66f206559716fd
Ke Chen <kechen1.ck@gmail.com>
2019-07-29 22:12:08 -0400

    using svm to decode neural activity

---
+++ classify_svm.m
@@ -0,0 +1,22 @@
+for j = 1:length(tastepsth)
+    X =[];
+    Y =[];
+    for i = 1:length(taste)
+        X{i} = tastepsth_bin25(j).(taste{i}).FRmatrix;
+        Y{i} = i*ones([size(X{i},1),1]);
+    end
+    
+    X = [X{1};X{2};X{3};X{4}];
+    Y = [Y{1};Y{2};Y{3};Y{4}];
+    
+    t = templateSVM('Standardize',true,'KernelFunction','linear');
+%     t = templateSVM('Standardize',true)
+
+%     Mdl = fitcecoc(X,Y,'OptimizeHyperparameters','auto',...
+%         'HyperparameterOptimizationOptions',struct('AcquisitionFunctionName',...
+%         'expected-improvement-plus'));
+    Md1 = fitcecoc(X,Y,'Learners',t, 'ClassNames',[1,2,3,4]);
+    CVMdl = crossval(Mdl);
+    genError(j) = kfoldLoss(CVMdl);
+    fprintf('Finish processing neuron # %0.f\n',j)
+end
