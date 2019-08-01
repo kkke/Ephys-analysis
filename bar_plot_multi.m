28a96662fe99e19f25f9238b45ae1071a4b43f77
Ke Chen <kechen1.ck@gmail.com>
2019-08-01 17:34:23 -0400

    bar plots for more than 3 groups

---
+++ bar_plot_multi.m
@@ -0,0 +1,18 @@
+%% This function is used for bar plots for mutiple classes
+% data should be in matrix
+function bar_plot_multi(X)
+val_m = mean(X,1);
+val_sem = std(X,1)./sqrt(size(X,1));
+figure;
+color = {'c','m','r','y','b','k','g'};
+hold on
+for i = 1:length(val_m)
+    bar(i,val_m(i),'FaceColor',color{i},'EdgeColor',[0,0,0],'LineWidth',1)
+end
+e=errorbar(val_m, val_sem,'LineWidth',1.5);
+ylim([0,2*max(val_m)])
+e.Color ='k';
+e.LineStyle = 'none';
+p = anova1(X);
+end
+
