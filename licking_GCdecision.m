acc8df2b7b52732886899e4f6c1c8aa83384adf6
Ke Chen <kechen1.ck@gmail.com>
2019-08-01 17:04:21 -0400

    add inter-lick interval

--- licking_GCdecision.m
+++ licking_GCdecision.m
@@ -1,6 +1,7 @@
 %%
 load('DataForLicking.mat')
 %% calculate the sampling duration for each tastants
+clearvars -except data
 for i = 1:length(data)
      temp = data(i).raw_decision; % extract the raw decision information
      for j = 1:size(temp,1)
@@ -15,19 +16,24 @@ for i = 1:length(data)
      clear sampling
 end
 
+data = iti_decision(data);
 
-%%%% summary the sampling duration for each tastant
+%%%% summary the sampling duration and InterLick Interval for each tastant
 for i = 1:length(data)
     id{i} = data(i).mouseID;
 end
 id_uni = unique(id);
-sd_f = {'S', 'M', 'SO', 'Q','sampling'}
+% sd_f = {'S', 'M', 'SO', 'Q','sampling'}
+sd_f = {'S', 'M', 'SO', 'Q'};
+
 for i = 1:length(id_uni)
     duration(i).id = id_uni{i};
+    iti(i).id      = id_uni{i};
     idx = find(strcmp(id,id_uni{i}));
     if length(idx) >=1
         for j = 1:length(sd_f)
             duration(i).(sd_f{j}) = mean([data(idx).(sd_f{j})]);
+            iti(i).(sd_f{j})      = mean([data(idx).([sd_f{j},'_iti'])]);
         end
     else
         error('Something is wrong')
@@ -35,6 +41,12 @@ for i = 1:length(id_uni)
 end
 clearvars -except data duration
 
+%% Save the results as table 
+iti = struct2table(iti);
+duration_temp = struct2table(duration);
+
+writetable(duration_temp,'Central-licking.xls','sheet',1)
+writetable(iti,'Central-licking.xls','sheet',2)
 
 %%% plot the result and perform the statistic test
 temp = duration;
@@ -158,6 +170,76 @@ e.LineStyle = 'none';
 % perform the t-test
 [h,p] = ttest2(temp(:,2),temp(:,4));
 ylim([0,2])
-%%
+%% calculate the performance for each tastant
 clearvars -except data duration timing
-save('DataForLicking.mat','data','duration','timing')
+for i = 1:length(data)
+     temp = data(i).raw_decision; % extract the raw decision information
+     performance = cell2mat(temp(:,3));
+     tasteID = unique(cell2mat(temp(:,10))); % find the tasteID line
+     taste = data(i).tasteID; % find the tasteID
+     for z = 1:length(taste)
+         t = cell2mat(temp(:,10));
+         p = performance(find(t == tasteID(z)));
+         data(i).([(taste{z}),'_p']) = sum(p)/length(p);  
+     end
+end
+
+
+%%%% summary the sampling duration for each tastant
+clear performance
+for i = 1:length(data)
+    id{i} = data(i).mouseID;
+end
+id_uni = unique(id);
+sd_f = {'S_p', 'M_p', 'SO_p', 'Q_p'}
+for i = 1:length(id_uni)
+    performance(i).id = id_uni{i};
+    idx = find(strcmp(id,id_uni{i}));
+    if length(idx) >=1
+        for j = 1:length(sd_f)
+            performance(i).(sd_f{j}) = mean([data(idx).(sd_f{j})]);
+        end
+    else
+        error('Something is wrong')
+    end
+end
+clearvars -except data duration timing performance
+
+
+%% plot the result and perform the statistic test
+temp = performance;
+temp = rmfield(temp,'id');
+temp = cell2mat(squeeze(struct2cell(temp))');
+
+control = temp;
+m1 = mean(control(:,1));
+m2 = mean(control(:,2));
+m3 = mean(control(:,3));
+m4 = mean(control(:,4));
+
+name = {[],'S','M','SO','Q',[]}
+sem1 = std(control(:,1))./sqrt(length(control(:,3)));
+sem2 = std(control(:,2))./sqrt(length(control(:,3)));
+sem3 = std(control(:,3))./sqrt(length(control(:,3)));
+sem4 = std(control(:,4))./sqrt(length(control(:,4)));
+
+figure;
+bar(1,m1,'EdgeColor',[0 0 0],'FaceColor',[0.5,0.5,1],'LineWidth',1);hold on
+bar(2,m2,'EdgeColor',[0 0 0],'FaceColor',[1,0.5,0.25],'LineWidth',1);hold on
+bar(3,m3,'EdgeColor',[0 0 0],'FaceColor',[1,0.5,0.25],'LineWidth',1);hold on
+bar(4,m4,'EdgeColor',[0 0 0],'FaceColor',[1,0.5,0.25],'LineWidth',1);hold on
+
+set(gca,'XTick',0:5)
+set(gca,'xticklabel',name)
+ylabel('Performance')
+hold on;
+e=errorbar([m1, m2, m3, m4], [sem1, sem2, sem3, sem4],'LineWidth',1.5);
+% plot([1 1],[mean(a_m) mean(a_m)+a_sem],'r');hold on;
+e.Color ='k';
+e.LineStyle = 'none';
+ylim([0,1])
+%% perform one-way anova
+temp = temp(:,1:4); % use compare duration for each tastant
+p = anova1(temp)
+%%
+ 
