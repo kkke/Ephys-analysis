3c1ccca787ac0eb531decf1a5b0db6c1ab48ad65
Ke Chen <kechen1.ck@gmail.com>
2019-08-02 09:59:35 -0400

    add stats argument input

--- bar_plot_multi.m
+++ bar_plot_multi.m
@@ -1,6 +1,9 @@
 %% This function is used for bar plots for mutiple classes
 % data should be in matrix
-function bar_plot_multi(X)
+function bar_plot_multi(X,stats)
+if nargin < 2
+    stats = false;
+end
 val_m = mean(X,1);
 val_sem = std(X,1)./sqrt(size(X,1));
 figure;
@@ -13,6 +16,12 @@ e=errorbar(val_m, val_sem,'LineWidth',1.5);
 ylim([0,2*max(val_m)])
 e.Color ='k';
 e.LineStyle = 'none';
-p = anova1(X);
+set(gca,'TickDir','out')
+set(gca,'fontsize',12)
+set(gca,'TickLengt', [0.01 0.01]);
+if stats
+    p = anova1(X);
+    print(p)
+end
 end
 
