d5640927c012274a7b49c00db9a8947874915447
Ke Chen <kechen1.ck@gmail.com>
2019-11-06 12:28:50 -0500

    add distance analysis

--- populationTrajectory_PCA.m
+++ populationTrajectory_PCA.m
@@ -26,20 +26,28 @@ for i = 1:length(Sum)
 end
 %% PCA analysis
 [coeff,score,latent,tsquared,explained,mu] = pca((firing_rate.unit_all)');
-% [coeff,score,latent,tsquared,explained,mu] = pca((firing_rate.unit_all(:,11:end))');
+[coeff,score,latent,tsquared,explained,mu] = pca((firing_rate.unit_all(:,11:end))');
 %% apply the PCA transformation to each case
-% S_traj = ((firing_rate.unit_S(:,11:end))'-mu)*coeff(:,1:3);
+S_traj = ((firing_rate.unit_S(:,11:end))'-mu)*coeff(:,1:3);
+Q_traj = ((firing_rate.unit_Q(:,11:end))'-mu)*coeff(:,1:3);
+M_traj = ((firing_rate.unit_M(:,11:end))'-mu)*coeff(:,1:3);
+SO_traj = ((firing_rate.unit_SO(:,11:end))'-mu)*coeff(:,1:3);
+
 S_traj = (firing_rate.unit_S'-mu)*coeff(:,1:3);
 Q_traj = (firing_rate.unit_Q'-mu)*coeff(:,1:3);
 M_traj = (firing_rate.unit_M'-mu)*coeff(:,1:3);
 SO_traj= (firing_rate.unit_SO'-mu)*coeff(:,1:3);
 
 figure;
-% plot3(S_traj(:,1),S_traj(:,2),S_traj(:,3))
-% hold on
-% plot3(Q_traj(:,1),Q_traj(:,2),Q_traj(:,3))
-% plot3(M_traj(:,1),M_traj(:,2),M_traj(:,3))
-% plot3(SO_traj(:,1),SO_traj(:,2),SO_traj(:,3))
+plot3(S_traj(:,1),S_traj(:,2),S_traj(:,3),'-ro')
+hold on
+plot3(Q_traj(:,1),Q_traj(:,2),Q_traj(:,3),'-bo')
+plot3(M_traj(:,1),M_traj(:,2),M_traj(:,3),'-mo')
+plot3(SO_traj(:,1),SO_traj(:,2),SO_traj(:,3),'-ko')
+box on
+grid on
+rotate3d on
+
 
 plot(S_traj(:,1),S_traj(:,2),'-ro')
 hold on
@@ -50,3 +58,75 @@ xlabel('Principal component 1')
 ylabel('principal component 2')
 legend({'S','Q','M','SO'})
 save('Trajectory_PCA.mat')
+%% calculate eucleadian distance in PCA space
+S_Qdist = sqrt(sum((S_traj - Q_traj).^2,2));
+M_SOdist = sqrt(sum((M_traj - SO_traj).^2,2));
+
+S_Mdist  = sqrt(sum((S_traj - M_traj).^2,2));
+Q_SOdist = sqrt(sum((Q_traj - SO_traj).^2,2));
+
+figure;
+hold on
+plot(data(1).unit_S.timepoint(11:end),S_Qdist,'-ro')
+plot(data(1).unit_S.timepoint(11:end),M_SOdist,'-bo')
+ylim([0,25])
+ylabel('Eucleadian distance in PCA space')
+xlabel('Time (s)')
+legend({'S-Q distance','M-SO distance'})
+
+figure;
+hold on
+plot(data(1).unit_S.timepoint(11:end),S_Mdist,'-mo')
+plot(data(1).unit_S.timepoint(11:end),Q_SOdist,'-ko')
+ylim([0,25])
+ylabel('Eucleadian distance in PCA space')
+xlabel('Time (s)')
+legend({'S-M distance','Q-SQ distance'})
+%%
+for i = 1:length(data)
+    data(i).SM = abs(psth_auROC_ke(data(i).unit_S.scmatrix,data(i).unit_M.scmatrix));
+    data(i).SQ = abs(psth_auROC_ke(data(i).unit_S.scmatrix,data(i).unit_Q.scmatrix));
+    data(i).SSO = abs(psth_auROC_ke(data(i).unit_S.scmatrix,data(i).unit_SO.scmatrix));
+    data(i).MSO = abs(psth_auROC_ke(data(i).unit_M.scmatrix,data(i).unit_SO.scmatrix));
+    data(i).QSO = abs(psth_auROC_ke(data(i).unit_Q.scmatrix,data(i).unit_SO.scmatrix));
+    data(i).MQ = abs(psth_auROC_ke(data(i).unit_M.scmatrix,data(i).unit_Q.scmatrix));
+    fprintf('Finish processing neuron # %0.1f\n', i)
+end
+
+for i = 1:length(data)
+    SQ(i,:) = data(i).SQ;
+    MSO(i,:) = data(i).MSO;
+    SM (i,:) = data(i).SM;
+    QSO(i,:) = data(i).QSO;
+    SSO(i,:) = data(i).SSO;
+    MQ(i,:)  = data(i).MQ;
+end
+
+
+similarAction = 1/2 * (SQ + MSO);
+m_similarAction = mean(similarAction);
+sem_similarAction = std(similarAction)/sqrt(size(similarAction,1));
+
+D_similarAction = 1/6 * (SM + QSO + SQ + MSO + SSO + MQ);
+D_similarAction = 1/4 * (SM + QSO + SSO + MQ);
+% D_similarAction = 1/2 * (SM + QSO);
+m_DsimilarAction = mean(D_similarAction);
+sem_DsimilarAction = std(D_similarAction)/sqrt(size(D_similarAction,1));
+
+figure; 
+h1 = boundedline(data(1).unit_all.timepoint, m_similarAction, sem_similarAction)
+hold on
+h3 = boundedline(data(1).unit_all.timepoint, m_DsimilarAction, sem_DsimilarAction,'r')
+xlim([0,2.5])
+ylim([0.06,0.12])
+xlabel('Time (s)')
+ylabel('Normalized auROC')
+
+
+for i = 1:size(similarAction,2)
+    [~, p(i)] = ttest2(similarAction(:,i),D_similarAction(:,i));
+end
+p = p*25;
+idx = p<0.05;
+time = data(1).unit_all.timepoint(idx);
+scatter(time,0.09* ones(size(time)),'ko')
