8113bd90fdcb1358008399cd0e798dc554428d4c
kkke <llkong.ck@gmail.com>
2020-07-21 16:57:36 -0400

    Calculate taste responses

--- TasteResponsivness_StatsandFigures.m
+++ TasteResponsivness_StatsandFigures.m
@@ -3,7 +3,7 @@
 % Extract Taste Responsivness
 % pre taste 500ms
 % post taste 500 ms
-
+addpath('C:\Users\Ke Chen\Dropbox\TasteDiscriminationAnalysis_withKe')
 load('Sum.mat');
 %%
 % Taste Responsivness calculated with Ranksum (baseline -500ms Vs. Evoked
@@ -18,7 +18,7 @@ for k = 1:size(Sum,2)
         tasteResp(k,4) = ranksum(reshape(Sum(k).PSTH.LeftRew.Quinine.FRmatrix(:,5:10),1,[]),...
                                  reshape(Sum(k).PSTH.LeftRew.Quinine.FRmatrix(:,11:16),1,[]));
                 for t=1:4
-                        if tasteResp(k,t)<0.01
+                        if tasteResp(k,t)<0.005
                             tasteFlag(k,t)=1;
                         else
                             tasteFlag(k,t)=0;
@@ -251,7 +251,7 @@ text(3-0.35,0.05,[num2str(sum(TotTasteSel(:,3))) '/' num2str(size(TotTasteSel,1)
 text(4-0.35,0.05,[num2str(sum(TotTasteSel(:,4))) '/' num2str(size(TotTasteSel,1))]);
 
 for f = 1:size(TotTasteSel,1)
-TotTasteSel(f,5)=sum(TotTasteSel(f,1:4));
+    TotTasteSel(f,5)=sum(TotTasteSel(f,1:4));
 end
 TotTasteSel2(1,1)=size(find(TotTasteSel(:,5)==1),1);
 TotTasteSel2(1,2)=size(find(TotTasteSel(:,5)==2),1);
@@ -263,7 +263,7 @@ TotTasteSel2(2,2)=TotTasteSel2(1,2)/size(TotTasteSel,1);
 TotTasteSel2(2,3)=TotTasteSel2(1,3)/size(TotTasteSel,1);
 TotTasteSel2(2,4)=TotTasteSel2(1,4)/size(TotTasteSel,1);
 f = size(TotTasteSel,1);
-clear TotTasteSel;clear H;
+
 
 subplot(1,2,2);
 bar(TotTasteSel2(2,:),'c');
@@ -272,6 +272,12 @@ text(1-0.35,0.025,[num2str(TotTasteSel2(1,1)) '/' num2str(f)]);
 text(2-0.35,0.025,[num2str(TotTasteSel2(1,2)) '/' num2str(f)]);
 text(3-0.35,0.025,[num2str(TotTasteSel2(1,3)) '/' num2str(f)]);
 text(4-0.35,0.025,[num2str(TotTasteSel2(1,4)) '/' num2str(f)]);
+
+figure;
+pie([size(TotTasteSel,1)/length(Sum),1-size(TotTasteSel,1)/length(Sum)])
+ylabel('Percentage of Taste Selective Neurons')
+
+% clear TotTasteSel;clear H;
 %%
 % Population decoding part
 H=1;
@@ -294,8 +300,180 @@ for j=1:214
      Pop(j).Quinine   =Sum(j).PSTH.LeftRew.Quinine.FRmatrix(:,:);
 
 end
+%% plot population PSTH for planning and goal
+%  Planning
+timepoint = (-6:0.1:2);
+timepoint = timepoint +0.01/2;
+timepoint(end)=[];
+id =1;
+id2=1;
+clear idRight idLeft
+for i = 1:length(Sum)
+     if Sum(i).auROC.Rcorr_vsLcorr.stats.respFlag ==0
+     elseif Sum(i).auROC.Rcorr_vsLcorr.p>0
+         idRight(id) = i;
+         id = id+1;
+     else
+         idLeft(id2) = i;
+         id2 = id2+1;
+     end     
+end
+figure;
+% N =1;
+baseline = find(timepoint>=-1 & timepoint <0);
+goal     = find(timepoint>=0 & timepoint <1);
+for k =1:length(idRight)
+       popM_Right(k,:) = Sum(idRight(k)).normPSTH.RightRew.Corr;
+       if mean(popM_Right(k,goal))> mean(popM_Right(k,baseline))
+           idRight_ex(k) = 1;
+       else
+           idRight_ex(k) = 0;
+       end
+           
+end
+clear k; clear N; N =1;
+for k =1:length(idLeft)
+       popM_Left(k,:) = Sum(idLeft(k)).normPSTH.LeftRew.Corr;
+       if mean(popM_Left(k,goal))> mean(popM_Left(k,baseline))
+           idLeft_ex(k) = 1;
+       else
+           idLeft_ex(k) = 0;
+       end          
+end
 
+figure(100) % population PSTHs for each individual taste
+subplot(2,2,1);
+%plot(mean(popM_exc(:,31:70))); hold on; plot(mean(popM_inh(:,31:70)));hold on;
+idL_ex  = find(idLeft_ex ==1);
+idL_inh = find(idLeft_ex ==0);
+id_ex   = find(idRight_ex ==1);
+id_inh  = find(idRight_ex ==0);
+%%
+figure
+boundedline(timepoint, mean(popM_Right(id_ex,:),1), std(popM_Right(id_ex,:),1)./sqrt(size(popM_Right(id_ex,:),1)));hold on;
+boundedline(timepoint, mean(popM_Right(id_inh,:),1), std(popM_Right(id_inh,:),1)./sqrt(size(popM_Right(id_inh,:),1)),'r');hold on;
+rectangle('Position',[0, min(mean(popM_Right(id_inh,:),1)),1,max(mean(popM_Right(id_ex,:),1))-min(mean(popM_Right(id_inh,:),1))])
+title('Right preferential neuron only')
+xlim([-1,2])
+%%
+figure
+boundedline(timepoint, mean(popM_Left(idL_ex,:),1), std(popM_Left(idL_ex,:),1)./sqrt(size(popM_Left(idL_ex,:),1)));hold on;
+boundedline(timepoint, mean(popM_Left(idL_inh,:),1), std(popM_Left(idL_inh,:),1)./sqrt(size(popM_Left(idL_inh,:),1)),'r');hold on;
+rectangle('Position',[0, min(mean(popM_Left(idL_inh,:),1)),1,max(mean(popM_Left(idL_ex,:),1))-min(mean(popM_Left(idL_inh,:),1))])
+title('Left preferential neuron only')
+xlim([-1,2])
+%%
+popM_exc = [];
+popM_inh = [];
+popM_exc =[popM_Left(idL_ex,:);popM_Right(id_ex,:)];
+popM_inh =[popM_Left(idL_inh,:);popM_Right(id_inh,:)];
+figure
+h1 = boundedline(timepoint, mean(popM_exc,1), std(popM_exc,1)./sqrt(size(popM_exc,1)));hold on;
+h2 = boundedline(timepoint, mean(popM_inh,1), std(popM_inh,1)./sqrt(size(popM_inh,1)),'r');hold on;
+rectangle('Position',[0, min(mean(popM_inh,1)),1,max(mean(popM_exc,1))-min(mean(popM_inh,1))])
+title('population PSTH for goal neurons')
+xlim([-1,2])
+%
+% boundedline(1:40, mean(popM_inh(:,31:70)), std(popM_inh(:,31:70))./sqrt(size(popM_inh,1)),'r')
+% plot([20 20],[0.4 0.7],'--k');
+legend([h1,h2],{'exc','inh'});
+xlabel('Time (s)'); ylabel('auROC');
+% xticks([0 10 20 30 40]);xticklabels({'-2','-1','0','1','2'});
+% title('PopPSTH - Maltose');
+%% population PSTH for planning 
+clearvars -except Sum
+timepoint = (-6:0.1:2);
+timepoint = timepoint +0.01/2;
+timepoint(end)=[];
+id =1;
+id2=1;
+clear idRight idLeft
+for i = 1:length(Sum)
+     if Sum(i).auROC.PlanRcorr_vsLcorr.stats.respFlag ==0
+     elseif Sum(i).auROC.Rcorr_vsLcorr.p>0
+         idRight(id) = i;
+         id = id+1;
+     else
+         idLeft(id2) = i;
+         id2 = id2+1;
+     end     
+end
+figure;
+% N =1;
+baseline = find(timepoint>=-6 & timepoint <-5);
+goal     = find(timepoint>=-1 & timepoint <0);
+for k =1:length(idRight)
+       popM_Right(k,:) = Sum(idRight(k)).normPSTH.RightRew.Corr;
+       if mean(popM_Right(k,goal))> mean(popM_Right(k,baseline))
+           idRight_ex(k) = 1;
+       else
+           idRight_ex(k) = 0;
+       end
+           
+end
+clear k; clear N; N =1;
+for k =1:length(idLeft)
+       popM_Left(k,:) = Sum(idLeft(k)).normPSTH.LeftRew.Corr;
+       if mean(popM_Left(k,goal))> mean(popM_Left(k,baseline))
+           idLeft_ex(k) = 1;
+       else
+           idLeft_ex(k) = 0;
+       end          
+end
+
+figure(100) % population PSTHs for each individual taste
+subplot(2,2,1);
+%plot(mean(popM_exc(:,31:70))); hold on; plot(mean(popM_inh(:,31:70)));hold on;
+idL_ex  = find(idLeft_ex ==1);
+idL_inh = find(idLeft_ex ==0);
+id_ex   = find(idRight_ex ==1);
+id_inh  = find(idRight_ex ==0);
+%
+figure
+boundedline(timepoint, mean(popM_Right(id_ex,:),1), std(popM_Right(id_ex,:),1)./sqrt(size(popM_Right(id_ex,:),1)));hold on;
+boundedline(timepoint, mean(popM_Right(id_inh,:),1), std(popM_Right(id_inh,:),1)./sqrt(size(popM_Right(id_inh,:),1)),'r');hold on;
+rectangle('Position',[0, min(mean(popM_Right(id_inh,:),1)),1,max(mean(popM_Right(id_ex,:),1))-min(mean(popM_Right(id_inh,:),1))])
+title('Right preferential neuron only')
+xlim([-4,2])
+%
+figure
+boundedline(timepoint, mean(popM_Left(idL_ex,:),1), std(popM_Left(idL_ex,:),1)./sqrt(size(popM_Left(idL_ex,:),1)));hold on;
+boundedline(timepoint, mean(popM_Left(idL_inh,:),1), std(popM_Left(idL_inh,:),1)./sqrt(size(popM_Left(idL_inh,:),1)),'r');hold on;
+rectangle('Position',[0, min(mean(popM_Left(idL_inh,:),1)),1,max(mean(popM_Left(idL_ex,:),1))-min(mean(popM_Left(idL_inh,:),1))])
+title('Left preferential neuron only')
+xlim([-4,2])
+%
+popM_exc = [];
+popM_inh = [];
+popM_exc =[popM_Left(idL_ex,:);popM_Right(id_ex,:)];
+popM_inh =[popM_Left(idL_inh,:);popM_Right(id_inh,:)];
+figure
+h1 = boundedline(timepoint, mean(popM_exc,1), std(popM_exc,1)./sqrt(size(popM_exc,1)));hold on;
+h2 = boundedline(timepoint, mean(popM_inh,1), std(popM_inh,1)./sqrt(size(popM_inh,1)),'r');hold on;
+rectangle('Position',[0, min(mean(popM_inh,1)),1,max(mean(popM_exc,1))-min(mean(popM_inh,1))])
+title('population PSTH for goal neurons')
+xlim([-4,2])
+%
+% boundedline(1:40, mean(popM_inh(:,31:70)), std(popM_inh(:,31:70))./sqrt(size(popM_inh,1)),'r')
+% plot([20 20],[0.4 0.7],'--k');
+legend([h1,h2],{'exc','inh'});
+xlabel('Time (s)'); ylabel('auROC');
+%%
+clearvars -except Sum
+timepoint = (-6:0.1:2);
+timepoint = timepoint +0.01/2;
+timepoint(end)=[];
+for i = 1:length(Sum)
+    popM_left(i,:) = Sum(i).normPSTH.LeftRew.Corr;
+    popM_right(i,:) = Sum(i).normPSTH.RightRew.Corr;
+end
+figure
+h1 = boundedline(timepoint, mean(popM_left,1), std(popM_left,1)./sqrt(size(popM_left,1)));hold on;
+figure
+h2 = boundedline(timepoint, mean(popM_right,1), std(popM_right,1)./sqrt(size(popM_right,1)),'r');hold on;
+rectangle('Position',[0, min(mean(popM_inh,1)),1,max(mean(popM_exc,1))-min(mean(popM_inh,1))])
 % % Decoder part
+
 % Neur   = 2;
 % trNumM = size(Sum(Neur).PSTH.RightRew.Maltose.spikeraster,2);
 % trNumO = size(Sum(Neur).PSTH.RightRew.Octacetate.spikeraster,2);
